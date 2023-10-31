82353a60bdbd7a6befdbcd2cef0931a5cb63ff46
Prima Putra Agusantara <primatara42@gmail.com>
2023-10-31 11:09:56 +0700

    #Progress 7 membuat xml daftar (masih berantakan)

--- app/src/main/java/com/pam/kas_dagang_makanan/LoginActivity.java
+++ app/src/main/java/com/pam/kas_dagang_makanan/LoginActivity.java
@@ -2,13 +2,53 @@ package com.pam.kas_dagang_makanan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    TextView tvPindahDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login();
+    }
+
+    private void login(){
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+        tvPindahDaftar = findViewById(R.id.tv_pindahDaftar);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(username)){
+                    etUsername.setError("Silahkan isi username anda!");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    etPassword.setError("Silahkan isi password anda!");
+                }
+            }
+        });
+
+        tvPindahDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/pam/kas_dagang_makanan/RegisterActivity.java
+++ app/src/main/java/com/pam/kas_dagang_makanan/RegisterActivity.java
@@ -3,12 +3,44 @@ package com.pam.kas_dagang_makanan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    TextView tvPindahLogin;
+    Button btnDaftar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        daftar();
+    }
+
+    private void daftar(){
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        tvPindahLogin = findViewById(R.id.tv_pindahLogin);
+        btnDaftar = findViewById(R.id.btn_daftar);
+
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(username)){
+                    etUsername.setError("Silahkan isi username anda!");
+                }
+                else if(TextUtils.isEmpty(password)){
+                    etPassword.setError("Silahkan isi password anda!");
+                }
+            }
+        });
     }
 }
